237fa9737378056d76db9f36eec3a7eb1bb46098
alex96jvm <alex96jvm@gmail.com>
2024-09-04 14:03:08 +0300

    Complete final task

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
-    public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-    }
+        public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+            Map<Integer, Set<String>> map = new HashMap<>();
+
+            for (String word : sourceMap.keySet()) {
+                int wordLength = word.length();
+                map.putIfAbsent(wordLength, new HashSet<>());
+                map.get(wordLength).add(word);
+            }
+            return map;
+        }
 }
